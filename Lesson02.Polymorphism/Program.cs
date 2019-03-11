6e956d7520c6f0c5a6d69453a60a33fe01ac5efc
Tim Albert <talbert1209@gmail.com>
2019-03-11 13:15:19 -0500

    Added polymorphism example.

---
+++ Lesson02.Polymorphism/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02.Polymorphism
+{
+    class Program
+    {
+        class Polygon
+        {
+            public virtual void Draw()
+            {
+                Console.WriteLine("Drawing: Polygon");
+            }
+        }
+
+        class Rectangle: Polygon
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("Drawing: Rectangle");
+            }
+        }
+
+        class Triangle: Polygon
+        {
+            public new void Draw()
+            {
+                Console.WriteLine("Drawing: Triangle");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //List<Polygon> polygons = new List<Polygon>();
+            //polygons.Add(new Polygon());
+            //polygons.Add(new Rectangle());
+            //polygons.Add(new Triangle());
+
+            //foreach (var polygon in polygons)
+            //{
+            //    polygon.Draw();
+            //}
+
+            //Showing display vs new keywords
+            var myTriangle = new Triangle();
+            Polygon myPolygon = myTriangle;
+
+            myPolygon.Draw();
+            myTriangle.Draw();
+        }
+    }
+}
